cec6f6ac326eb00bad414a922aef0e0c390c177d
Nhat Quang <nhatquang238@me.com>
2015-11-04 09:40:16 +0800

    feat(scraper) fire every request with a different IP address

--- README.md
+++ README.md
@@ -18,6 +18,7 @@ A fun project to help myself search, view reviews and find source to buy cheaper
 3. Potentially have to make your 1st scrape less suspicious. Do a sleep in between?
 4. Alright let's do this! Gotta catch'em by suprise. Need to get a sample board game object to design the db schema (postgres or rethink?). After your db setup, try a scrape with less than 10 pages and measure the time takes to complete. After that benchmarking, if everything is fine then just HIT it!
 5. On db choice, rethink seems to be a good start since i'm not so sure of the relationship between objects now.
+6. Soooo, changing ip for every new request with Tor to get around that annoying ip limit on boardgamegeek.com. However, it turns out that 14/100 requests are still 503... Are they dying everytime i crawl or are they actually analyze traffic pattern??? And estimate time to crawl 80k boardgames with 1 tor instance is 225hrs LOL. This has already taken into account ip change rate limit by Tor...
 
 ## DB Design
 1. Splitting up boardgame object from boardgamegeek.com
@@ -30,3 +31,6 @@ A fun project to help myself search, view reviews and find source to buy cheaper
 		 - boardgamedesigner
 		 - boardgameartist
 		 - boardgamepublisher
+		 
+## Challenges
+1. Seems like boardgamegeek.com is smarter than I think. 503 on 30% of request. Tried throttling but didn't work. Assumed they have some sort of IP rate limit. Resort to using Tor for making anonimous requests. However, tor requests also seem to exit from the same IP everytime so didn't solve the problem. What if I can change Tor Exit IP for every request? Or creating multiple tor instances and some how load balance requests to them?
--- scraper.js
+++ scraper.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 require('babel-core').transform('code');
 
 const bgg = require('bgg');
@@ -8,11 +8,25 @@ const cheerio = require('cheerio');
 const db = require('./lib/db');
 const logger = require('debug')('scraper');
 const Promise = require('bluebird');
+const Agent = require('socks5-https-client/lib/Agent');
+const chalk = require('chalk');
+const fs = require('fs');
+const Nightcrawler = new require('nightcrawler');
 
 const fullScrapeURI = 'https://boardgamegeek.com/browse/boardgame';
 const basePageURI = 'https://boardgamegeek.com/browse/boardgame/page/';
 const BATCHSIZE = 1;
+const nightcrawlerOpts = {
+		proxy: 'http://localhost:8123',
+		tor: {
+				password: 'torStrong123',
+				controlPort: 9051
+		}
+};
+const nightcrawler = new Nightcrawler(nightcrawlerOpts);
+
 var boardgames = [];
+var totalMiss = 0;
 
 function log(input, depth) {
 		if (typeof depth === 'undefined') {
@@ -24,7 +38,11 @@ function log(input, depth) {
 function fetchBoardGameByPageNumber(pageNumber) {
 		var options = {
 				uri: pageNumber,
-				baseUrl: basePageURI
+				baseUrl: basePageURI,
+				agentClass: Agent,
+				agentOptions: {
+						socksPort: 9050
+				}
 		};
 
 		return rp(options);
@@ -33,9 +51,9 @@ function fetchBoardGameByPageNumber(pageNumber) {
 function getAllBoardGameIdsOnPage($, boardgameIds) {
 		var re = /\/boardgame\/(\d+)\//;
 		
-		$('.collection_objectname a').each((index, elem) => {
-				let bgLink = $(elem).attr('href');
-				let id = re.exec(bgLink)[1];
+		$('.collection_objectname a').each(function (index, elem) {
+				var bgLink = $(elem).attr('href');
+				var id = re.exec(bgLink)[1];
 				
 				boardgameIds.push(id);
 		});
@@ -60,7 +78,7 @@ function scrapeAllBoardGameIds() {
 
 						// testing promise control flow
 						var futures = [];
-						for (let i=0; i < 3; i++) {
+						for (var i=1; i < 2; i++) {
 								futures.push(fetchBoardGameByPageNumber(i.toString()));
 						}
 						return futures;
@@ -70,9 +88,17 @@ function scrapeAllBoardGameIds() {
 						logger('%s directory pages were downloaded', reses.length);
 						var boardgameIds = [];
 						reses.forEach(function(res) {
-								let $ = cheerio.load(res);
+								var $ = cheerio.load(res);
 								
 								boardgameIds = getAllBoardGameIdsOnPage($, boardgameIds);
+						});
+
+						fs.writeFile('boardgameIds.txt', boardgameIds.join('\n'), function(err) {
+								if (err) {
+										logger(chalk.red('failed to write to file'));
+								}
+								
+								logger(chalk.green('all boardgame ids are saved to boardgameIds.txt'));
 						});
 
 						scrape(boardgameIds);
@@ -103,7 +129,7 @@ function scrapeAllBoardGameIds() {
 // 				boardgame.maxplaytime = boardgame.maxplaytime.value;
 // 				boardgame.playingtime = boardgame.playingtime.value;
 // 				boardgame.yearpublished = boardgame.yearpublished.value;
-				
+
 // 				delete boardgame.id;
 
 // 				db.saveBoardgame(boardgame, function(err, id) {
@@ -113,46 +139,57 @@ function scrapeAllBoardGameIds() {
 // 				});
 // 		});
 
-scrapeAllBoardGameIds();
-
 function scrape(boardgameIds) {
-		var batch = boardgameIds.splice(0, BATCHSIZE);
-		var futures = [];
+		nightcrawler.changeIp().then(function(ip) {
+				logger('switching to a new ip: %s', ip);
 
-		for (let i=0; i < batch.length; i++) {
-				futures.push(fetchBoardGameById(batch[i]));
-		}
+				_scrapeCb();
+		});
 
-		Promise.all(futures)
-				.then(function (reses) {
-						reses.forEach(function (res) {
-								var boardgame = res.items.item;
-								boardgame.name = boardgame.name[0].value;
-								boardgame.maxplayers = boardgame.maxplayers.value;
-								boardgame.maxplaytime = boardgame.maxplaytime.value;
-								boardgame.minage = boardgame.minage.value;
-								boardgame.minplayers = boardgame.minplayers.value;
-								boardgame.maxplaytime = boardgame.maxplaytime.value;
-								boardgame.playingtime = boardgame.playingtime.value;
-								boardgame.yearpublished = boardgame.yearpublished.value;
-				
-								delete boardgame.id;
-								
-								boardgames.push(boardgame);
-						});
+		function _scrapeCb() {
+				var batch = boardgameIds.splice(0, BATCHSIZE);
+				var futures = [];
 
-						if (boardgameIds.length === 0) {
-								logger('%s boardgames downloaded', boardgames.length);
-						} else {
-								logger('%s boardgames remaining...', boardgameIds.length);
+				for (var i=0; i < batch.length; i++) {
+						futures.push(fetchBoardGameById(batch[i]));
+				}
+
+				Promise.all(futures)
+						.then(function (reses) {
+								reses.forEach(function (res) {
+										var boardgame = res.items.item;
+										boardgame.name = boardgame.name[0].value;
+										boardgame.maxplayers = boardgame.maxplayers.value;
+										boardgame.maxplaytime = boardgame.maxplaytime.value;
+										boardgame.minage = boardgame.minage.value;
+										boardgame.minplayers = boardgame.minplayers.value;
+										boardgame.maxplaytime = boardgame.maxplaytime.value;
+										boardgame.playingtime = boardgame.playingtime.value;
+										boardgame.yearpublished = boardgame.yearpublished.value;
+										
+										delete boardgame.id;
+										
+										boardgames.push(boardgame);
+								});
+
+								if (boardgameIds.length === 0) {
+										logger('--------------------------------------');
+										logger('%s boardgames downloaded', boardgames.length);
+								} else {
+										logger('%s boardgames remaining...', boardgameIds.length);
+										scrape(boardgameIds);
+								}
+						})
+						.catch(function (err) {
+								totalMiss += 1;
+								logger(chalk.red('missing a boardgame!!! total miss is now: %s'), totalMiss);
 								scrape(boardgameIds);
-						}
-				})
-				.catch(function (err) {
-						scrape(boardgameIds);
-				});
+						});				
+		}
 }
 
 function fetchBoardGameById(id) {
 		return bgg('thing', {id: id, type: 'boardgame', videos: '1'});
 }
+
+scrapeAllBoardGameIds();
