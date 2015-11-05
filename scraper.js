3350bc83cedfeddbf193dd6c8379e9d263bf5b6c
Nhat Quang <nhatquang238@me.com>
2015-11-05 23:04:33 +0800

    feat(scraper) first version with 100% success rate

--- scraper.js
+++ scraper.js
@@ -7,7 +7,8 @@ const cheerio = require('cheerio');
 const db = require('./lib/db');
 const logger = require('debug')('scraper');
 const Promise = require('bluebird');
-const Agent = require('socks5-https-client/lib/Agent');
+const HTTPSAgent = require('socks5-https-client/lib/Agent');
+const HTTPAgent = require('socks5-http-client/lib/Agent');
 const chalk = require('chalk');
 const fs = require('fs');
 const randomUA = require('random-useragent');
@@ -16,8 +17,8 @@ const parser = require('xml2json');
 
 const fullScrapeURI = 'https://boardgamegeek.com/browse/boardgame';
 const basePageURI = 'https://boardgamegeek.com/browse/boardgame/page/';
-const bggAPIRoot = 'https://boardgamegeek.com/xmlapi2/';
-const BATCHSIZE = 10;
+const bggAPIRoots = ['https://boardgamegeek.com/xmlapi2/', 'http://www.boardgamegeek.com/xmlapi2/', 'http://wwww.rpggeek.com/xmlapi2/', 'http://www.videogamegeek.com/xmlapi2/'];
+const BATCHSIZE = 1;
 const nightcrawlerOpts = {
 		proxy: 'http://localhost:8123',
 		tor: {
@@ -30,11 +31,15 @@ const nightcrawler = new Nightcrawler(nightcrawlerOpts);
 var boardgames = [];
 var totalMiss = 0;
 
+function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function fetchBoardGameByPageNumber(pageNumber) {
 		var options = {
 				uri: pageNumber,
 				baseUrl: basePageURI,
-				agentClass: Agent,
+				agentClass: HTTPSAgent,
 				agentOptions: {
 						socksPort: 9050
 				},
@@ -52,11 +57,11 @@ function getAllBoardGameIdsOnPage($, boardgameIds) {
 		$('.collection_objectname a').each(function (index, elem) {
 				var bgLink = $(elem).attr('href');
 				var id = re.exec(bgLink)[1];
-				
+
 				boardgameIds.push(id);
 		});
 
-		return boardgameIds;		
+		return boardgameIds;
 }
 
 function scrapeAllBoardGameIds() {
@@ -67,8 +72,8 @@ function scrapeAllBoardGameIds() {
 				transform: function(res) {
 						return cheerio.load(res);
 				}
-		};		
-		
+		};
+
 		rp(options)
 				.then(function($) {
 						var pagesCount = $('.infobox a[title="last page"]').html();
@@ -87,7 +92,7 @@ function scrapeAllBoardGameIds() {
 						var boardgameIds = [];
 						reses.forEach(function(res) {
 								var $ = cheerio.load(res);
-								
+
 								boardgameIds = getAllBoardGameIdsOnPage($, boardgameIds);
 						});
 
@@ -95,7 +100,7 @@ function scrapeAllBoardGameIds() {
 								if (err) {
 										logger(chalk.red('failed to write to file'));
 								}
-								
+
 								logger(chalk.green('all boardgame ids are saved to boardgameIds.txt'));
 						});
 
@@ -103,15 +108,11 @@ function scrapeAllBoardGameIds() {
 				})
 				.catch(function(err) {
 						logger(err);
-				});		
+				});
 }
 
 function scrape(boardgameIds) {
-		nightcrawler.changeIp().then(function(ip) {
-				logger('switching to a new ip: %s', ip);
-
-				_scrapeCb();
-		});
+		_scrapeCb();
 		
 		function _scrapeCb() {
 				var batch = boardgameIds.splice(0, BATCHSIZE);
@@ -122,57 +123,79 @@ function scrape(boardgameIds) {
 						futures.push(fetchBoardGameById(batch[i]));
 				}
 
-				Promise.all(futures)
-						.then(function (reses) {
-								reses.forEach(function (res) {
-										var boardgame = parseBoardgame(res);
-										db.saveBoardgame(boardgame, function(err, key) {
-												if (!err) {
-														logger('saved as %s', key);
-														boardgames.push(key);
-														hitCount += 1;
-												}
-										});
-								});
+				Promise
+						.each(futures, function(val, index, length) {
+								var boardgame = parseBoardgame(val);
 
-								if (boardgameIds.length === 0) {
-										logger('--------------------------------------');
-										logger(chalk.green('%s boardgames downloaded'), boardgames.length);
-								} else {
-										logger('%s boardgames remaining...', boardgameIds.length);
-										scrape(boardgameIds);
-								}
+								db.saveBoardgame(boardgame, function(err, key){
+										if (!err) {
+												logger('saved as %s', key);
+												boardgames.push(key);
+										}
+								});
 						})
-						.catch(function (err) {
-								totalMiss += (BATCHSIZE - hitCount);
-								logger(chalk.red('missing boardgame!!! total miss is now: %s'), totalMiss);
-								scrape(boardgameIds);
-						});				
+								.then(function(res) {
+										if (boardgameIds.length === 0) {
+												logger('--------------------------------------');
+												logger(chalk.green('%s boardgames downloaded'), boardgames.length);
+										} else {
+												logger('%s boardgames remaining...', boardgameIds.length);
+												scrape(boardgameIds);
+										}
+								})
+						.catch(function(err) {
+								if ((typeof err.options === "undefined") || (typeof err.options.qs === "undefined") || (typeof err.options.qs.id === "undefined")) {
+										logger(err);
+								} else {
+										var failedId = err.options.qs.id;
+										
+										nightcrawler.changeIp().then(function(ip) {
+												logger('switching to a new ip: %s', ip);
+												logger('retry %s', failedId);
+												boardgameIds.unshift(failedId);
+												scrape(boardgameIds);
+										});
+								}
+						});
 		}
 }
 
 function fetchBoardGameById(id) {
+		var url = bggAPIRoots[getRandomInt(0, bggAPIRoots.length)];
+		logger('root url: %s', url);
+		var agent;
+		if (url.startsWith('https')) {
+				agent = HTTPSAgent;
+		} else {
+				agent = HTTPAgent;
+		}
+
 		return rp({
-				uri: bggAPIRoot + 'thing/',
+				uri: url + 'thing/',
 				qs: {
 						id: id,
 						type: 'boardgame',
 						videos: 1
 				},
-				agentClass: Agent,
+				agentClass: agent,
 				agentOptions: {
 						socksPort: 9050
 				},
 				headers: {
 						'User-Agent': randomUA.getRandom()
-				}				
+				}
 		});
 }
 
 function parseBoardgame(res) {
 		var boardgame = JSON.parse(parser.toJson(res)).items.item;
 
-		boardgame.name = boardgame.name[0].value || null;
+		if (Object.prototype.toString.call(boardgame.name) === '[object Array]') {
+				boardgame.name = boardgame.name[0].value || null;
+		} else {
+				boardgame.name = boardgame.name.value || null;
+		}
+		
 		boardgame.maxplayers = boardgame.maxplayers.value || null;
 		boardgame.maxplaytime = boardgame.maxplaytime.value || null;
 		boardgame.minage = boardgame.minage.value || null;
@@ -180,7 +203,7 @@ function parseBoardgame(res) {
 		boardgame.maxplaytime = boardgame.maxplaytime.value || null;
 		boardgame.playingtime = boardgame.playingtime.value || null;
 		boardgame.yearpublished = boardgame.yearpublished.value || null;
-		
+
 		delete boardgame.id;
 
 		return boardgame;
@@ -188,7 +211,13 @@ function parseBoardgame(res) {
 
 function run() {
 		db.setup();
-		scrapeAllBoardGameIds();		
+		logger("done db setup...");
+		scrapeAllBoardGameIds();
 }
 
 run();
+
+// fetchBoardGameById('110327')
+// 		.then(function(res) {
+// 				console.log(JSON.parse(parser.toJson(res)).items.item);
+// 		});
